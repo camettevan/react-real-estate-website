938c26bf10cc5e0f8daf9b3d76a16497250d85c5
Camette Van <camettevan@gmail.com>
2019-12-16 22:32:55 -0800

    first commit

---
+++ assets/js/realEstate/Header.js
@@ -0,0 +1,26 @@
+import React, { Component} from 'react'
+
+export default class Header extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+
+  render () {
+    return (
+      <header>
+        <div className="logo">
+          RLE
+        </div>
+        <nav>
+          <a href="#">Create ads</a>
+          <a href="#">About us</a>
+          <a href="#">Log in</a>
+          <a href="#" className="register-btn">Register</a>
+        </nav>
+      </header>
+    )
+  }
+}
---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,116 @@
+var listingsData = [
+  {
+    address: '20-34 Grand Ave',
+    city: 'Ridgewood',
+    state: 'NY',
+    rooms: 3,
+    price: 220000,
+    floorSpace: 2000,
+    extras: {
+      elevators: false,
+      swimming_pool: true,
+      finished_basement: true,
+      gym: true
+    },
+    homeType: 'Apartment',
+    image: 'https://cdngeneral.rentcafe.com/dmslivecafe/3/1021255/Windsor%20Falls%20-%20Raleigh%20Apartments.jpg?crop=(0,8.749999999999716,300,200)&cropxunits=300&cropyunits=200&quality=85&scale=both&'
+  },
+  {
+    address: '3 Universal Street',
+    city: 'Orlando',
+    state: 'FL',
+    rooms: 2,
+    price: 25000,
+    floorSpace: 1400,
+    extras: {
+      elevators: true,
+      swimming_pool: true,
+      finished_basement: false,
+      gym: true
+    },
+    homeType: 'Condo',
+    image: 'https://cdngeneral.rentcafe.com/dmslivecafe/3/510265/model-kitchen-north-austin-apartments(7).JPG?crop=(0,8.749999999999716,300,200)&cropxunits=300&cropyunits=200&quality=85&scale=both&'
+  },
+  {
+    address: '1 Lincoln Street',
+    city: 'Richmond',
+    state: 'VA',
+    rooms: 1,
+    price: 350000,
+    floorSpace: 2400,
+    extras: {
+      elevators: true,
+      swimming_pool: true,
+      finished_basement: false,
+      gym: true
+    },
+    homeType: 'Single Home',
+    image: 'https://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4206-28/the-kelvin-apartments-exterior.jpg'
+  },
+  {
+    address: '889 Beemore Street',
+    city: 'Newark',
+    state: 'NJ',
+    rooms: 3,
+    price: 80000,
+    floorSpace: 1000,
+    extras: {
+      elevators: false,
+      swimming_pool: true,
+      finished_basement: true,
+      gym: false
+    },
+    homeType: 'Multi Home',
+    image: 'https://cdngeneral.rentcafe.com/dmslivecafe/3/984046/Apartments%20on%20the%20Green%20Couch%20Seating.jpg?quality=85&scale=both&'
+  },
+  {
+    address: '53 Hollywood Blvd',
+    city: 'Los Angeles',
+    state: 'CA',
+    rooms: 3,
+    price: 600000,
+    floorSpace: 3000,
+    extras: {
+      elevators: false,
+      swimming_pool: true,
+      finished_basement: true,
+      gym: true
+    },
+    homeType: 'Condo',
+    image: 'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,fl_lossy/v1548955387/g5/g5-c-ipa80i9e-robbins-property-associates/g5-cl-1i4tf0rctl-arbor-village/uploads/01_gykx7a.jpg'
+  },
+  {
+    address: '2 Main Street',
+    city: 'Brooklyn',
+    state: 'NY',
+    rooms: 2,
+    price: 220000,
+    floorSpace: 2000,
+    extras: {
+      elevators: false,
+      swimming_pool: true,
+      finished_basement: false,
+      gym: false
+    },
+    homeType: 'Apartment',
+    image: 'https://cdngeneral.rentcafe.com/dmslivecafe/3/486043/wl-138vsweb.jpg?crop=(2,4.749999999999659,290.3809523809522,194)&cropxunits=300&cropyunits=193&quality=85&scale=both&'
+  },
+  {
+    address: '73 Gates Ave',
+    city: 'Springfield',
+    state: 'IN',
+    rooms: 0,
+    price: 150000,
+    floorSpace: 2000,
+    extras: {
+      elevators: false,
+      swimming_pool: false,
+      finished_basement: true,
+      gym: false
+    },
+    homeType: 'Studio',
+    image: 'https://cdngeneral.rentcafe.com/dmslivecafe/3/486043/wl-138vsweb.jpg?crop=(2,4.749999999999659,290.3809523809522,194)&cropxunits=300&cropyunits=193&quality=85&scale=both&'
+  }
+]
+
+export default listingsData;
